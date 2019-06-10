6042beb297872e156e5a9dde6a12088fcfe9ee85
Jani Palsamäki <jani.palsamaki@gmail.com>
2019-06-10 22:33:24 +0300

    Updates

---
+++ precache-manifest.4d41f0e931b2d087b70dfaf440245300.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601772707693fb51f2db199806a8a4e9",
+    "url": "/hearthstone-app/index.html"
+  },
+  {
+    "revision": "bf478e3bc93c31905e1e",
+    "url": "/hearthstone-app/static/css/2.4058c193.chunk.css"
+  },
+  {
+    "revision": "26ef274a5288833c2d84",
+    "url": "/hearthstone-app/static/css/main.0a09996e.chunk.css"
+  },
+  {
+    "revision": "bf478e3bc93c31905e1e",
+    "url": "/hearthstone-app/static/js/2.0ce87174.chunk.js"
+  },
+  {
+    "revision": "26ef274a5288833c2d84",
+    "url": "/hearthstone-app/static/js/main.41a1c1df.chunk.js"
+  },
+  {
+    "revision": "a4a20e05f88fe0304097",
+    "url": "/hearthstone-app/static/js/runtime~main.5122ae21.js"
+  },
+  {
+    "revision": "53582c6134991c5726e393ef5f47af77",
+    "url": "/hearthstone-app/static/media/logo.53582c61.svg"
+  },
+  {
+    "revision": "0f5c74d64bd66b0f2965a9e29ee4511b",
+    "url": "/hearthstone-app/static/media/spinner.0f5c74d6.gif"
+  }
+]);
